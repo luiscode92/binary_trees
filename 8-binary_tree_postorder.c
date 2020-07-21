c9347773b3bb200dcd632b2a8e7cae6af7485169
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-07-21 17:35:02 -0500

    betty fixed

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
-
+/**
+  *binary_tree_postorder - function for postorder tree
+  *@tree: the tree
+  *@func: pointer to a function to call for each node
+  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree != NULL && func != NULL)
