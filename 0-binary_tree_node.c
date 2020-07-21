345d71730a05dd7da48a5cecb70d430aac59c3d7
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-07-21 14:13:59 -0500

    betty fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,20 +1,20 @@
 #include "binary_trees.h"
 /**
- *bynary_tree_node -function that creates a binary tree node
+ *binary_tree_node - function that creates a binary tree node
  *@parent: parent node
  *@value: value
- *Return: new node 
+ *Return: new node
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *rootNode = NULL;
 
-  rootNode = calloc(1, sizeof(binary_tree_t));
-  if (rootNode == NULL)
-	  return (NULL);
-  rootNode->parent = parent;
-  rootNode->n = value;
-  rootNode->left = NULL;
-  rootNode->right = NULL;
-  return (rootNode);
+	rootNode = calloc(1, sizeof(binary_tree_t));
+	if (rootNode == NULL)
+		return (NULL);
+	rootNode->parent = parent;
+	rootNode->n = value;
+	rootNode->left = NULL;
+	rootNode->right = NULL;
+	return (rootNode);
 }
