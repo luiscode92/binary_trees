d3e26d4c27cba334b1e57de8f99287feacbe8c1e
Luis Enrique Sanjuan Melo <luiscode92@gmail.com>
2020-07-21 11:37:54 -0500

    add binary tree right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_insert_right - function that inserts a node as the left-child of another node
+ *@parent: parent node
+ *@value: value of the node
+ *Return: pointer to the created node 
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *rootNode = NULL;
+
+	if (parent == NULL)
+			return (NULL);
+	rootNode = calloc(1, sizeof(binary_tree_t));
+	if (rootNode == NULL)
+			return (NULL);
+	rootNode->parent = parent;
+	rootNode->n = value;
+	if (parent->right != NULL)
+	{
+		rootNode->right = parent->right;
+		rootNode->right->parent = rootNode;
+	}
+	parent->right = rootNode;
+	return (rootNode);
+}
