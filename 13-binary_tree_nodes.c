b6c308472eab524dcac7aa4dfae1704653442bd2
Luis Sanjuan <luiscode92@gmail.com>
2020-07-22 15:35:39 -0500

    betty fixed

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,7 +5,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t count = 0;
 
 	if (tree == NULL)
-		return(0);
+		return (0);
 
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
